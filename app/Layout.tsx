4342b79ecdc140488146a534789015942f180d11
projectgz <pjt1gz@gmail.com>
2025-02-11 17:46:37 +0900

    [25-02-11-화] sidebar추가된 layout분리. script 컴포넌트 추가

---
+++ app/Layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
+import { AppSidebar } from "./components/app-sidebar";
+
+export function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <SidebarProvider>
+            <AppSidebar />
+            <main>
+                <SidebarTrigger />
+                {/* 아래 children에 현재 라우트에 매핑된 컴포넌트를 표시한다. */}
+                {children}
+            </main>
+        </SidebarProvider>
+    )
+}
--- app/root.tsx
+++ app/root.tsx
@@ -8,8 +8,8 @@ import {
 import type { LinksFunction } from "@remix-run/node";
 
 import "./tailwind.css";
-import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
-import { AppSidebar } from "./components/app-sidebar";
+
+import { Layout } from "./Layout";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -24,9 +24,9 @@ export const links: LinksFunction = () => [
   },
 ];
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export default function App() {
   return (
-    <html lang="en">
+    <html lang="ko" suppressHydrationWarning>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -34,21 +34,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        <SidebarProvider>
-          <AppSidebar />
-          <main>
-            <SidebarTrigger />
-            {/* 아래 children에 현재 라우트에 매핑된 컴포넌트를 표시한다. */}
-            {children} 
-          </main>
-        </SidebarProvider>
+        <Layout>
+          <Outlet />
+        </Layout>
+        <ScrollRestoration />
+        <Scripts />
       </body>
     </html>
   );
-}
-
-export default function App() {
-  // 여기서 라우트에 맵핑된 컴포넌트를 표시한다.
-  // Outlet은 Layout으로 자동으로 감싸진다.
-  return <Outlet />;
 }
